cc188a4392df10820563d954ed77857a3cbb8e56
Ahmed Hatem <54455354+AhmedHatemS@users.noreply.github.com>
2021-02-24 23:53:58 +0200

    Add files via upload

---
+++ BinarySearchTree (BST)/BST.h
@@ -0,0 +1,25 @@
+#ifndef BST_H_INCLUDED
+#define BST_H_INCLUDED
+
+typedef char EntryType;
+typedef struct node{
+    EntryType Info;
+    struct node *Left;
+    struct node *Right;
+}Node;
+typedef Node *Tree;
+
+void CreateTree(Tree *t);
+int TreeEmpty(Tree *t);
+int TreeFull(Tree *t);
+void InOrder(Tree t, void(*pvisit)(Entry));
+void PreOrder(Tree t, void(*pvisit)(Entry));
+void PostOrder(Tree t, void(*pvisit)(Entry));
+int TreeSize(Tree t);
+int TreeHeight(Tree t);
+void PrintLevel(Tree *t, int level, void(*pvisit)(Entry));
+void Insert(Tree *t,EntryType item);
+void Insert(Tree *t,EntryType item);
+int Delete(Tree *t,EntryType k);
+
+#endif // BST_H_INCLUDED
